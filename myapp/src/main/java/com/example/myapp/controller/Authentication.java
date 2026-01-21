3f9e4b4b795ec5e30a516cffdf8d880de0201078
harsh-456-18 <depaharshitha@gmail.com>
2026-01-21 13:44:28 +0530

    Read all the data

--- myapp/src/main/java/com/example/myapp/controller/Authentication.java
+++ myapp/src/main/java/com/example/myapp/controller/Authentication.java
@@ -1,5 +1,7 @@
 package com.example.myapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,4 +38,11 @@ public class Authentication {
 
         return "signup sucess";
     }
+
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return db.findAll();
+    }
+    
 }
